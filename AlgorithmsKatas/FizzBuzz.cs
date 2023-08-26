9e44acff4e41aa7f73e9bd44a6f9e8b93c7ad3f1
Bartosz Sitek <bartosz.sitek.97@gmail.com>
2023-08-26 19:54:24 +0200

    added fizzbuzz kata with tests

---
+++ AlgorithmsKatas/FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿namespace AlgorithmsKatas
+{
+    public class FizzBuzz
+    {
+        //https://leetcode.com/problems/fizz-buzz/
+        public IList<string> FizzBuzzes(int n)
+        {
+            List<string> result = new();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    result.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    result.Add("Buzz");
+                }
+                else
+                {
+                    result.Add(i.ToString());
+                }
+            }
+
+            return result;
+        }
+
+    }
+
+}
---
+++ AlgorithmsKatasTests/FizzBuzzTest.cs
@@ -0,0 +1,28 @@
+﻿using AlgorithmsKatas;
+using Xunit;
+
+namespace AlgorithmsKatasTests
+{
+    public class FizzBuzzTest
+    {
+        private readonly FizzBuzz _sut;
+        public FizzBuzzTest()
+        {
+            _sut = new FizzBuzz();
+        }
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        [InlineData(3, "Fizz")]
+        [InlineData(4, "4")]
+        [InlineData(5, "Buzz")]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(30, "FizzBuzz")]
+        public void FizzBuzzTests(int n, string expected)
+        {
+            var result = _sut.FizzBuzzes(n);
+            //n-1 because our iteration starts at index 1, not 0
+            Assert.Equal(expected, result[n - 1]);
+        }
+    }
+}
