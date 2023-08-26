0e852a2c8a110c185d2222f905872d8c72ed5c05
Bartosz Sitek <bartosz.sitek.97@gmail.com>
2023-08-26 15:51:05 +0200

    contains duplicate

--- AlgorithmsKatas/ContainsDuplicate.cs
+++ AlgorithmsKatas/ContainsDuplicate.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace AlgorithmsKatas
 {
-    internal class ContainsDuplicate
+    //https://leetcode.com/problems/contains-duplicate/
+    public class ContainsDuplicate
     {
+        public bool ContainsDuplicates(int[] nums)
+        {
+            var dictionary = new Dictionary<int, int>();
+            //List<int> list = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dictionary.ContainsKey(nums[i]))
+                {
+                    return true;
+                }
+                dictionary[nums[i]] = i; // putting number in the dictionary
+            }
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (list.Contains(nums[i]))
+            //    {
+            //        return true;
+            //    }
+            //    list.Add(nums[i]);
+            //}
+
+            return false;
+        }
+
     }
 }
--- AlgorithmsKatasTests/ContainsDuplicateTest.cs
+++ AlgorithmsKatasTests/ContainsDuplicateTest.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using AlgorithmsKatas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace AlgorithmsKatasTests
 {
-    internal class ContainsDuplicateTest
+    public class ContainsDuplicateTest
     {
+        private readonly ContainsDuplicate _sut;
+
+        public ContainsDuplicateTest()
+        {
+            _sut = new ContainsDuplicate();
+        }
+        [Fact]
+        public void ContainsDuplicateTests()
+        {
+           
+            var result1 = _sut.ContainsDuplicates(new int[] { 1, 2, 3, 1 });
+            var result2 = _sut.ContainsDuplicates(new int[] { 1, 2, 3, 4 });
+            var result3 = _sut.ContainsDuplicates(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 });
+            var result4 = _sut.ContainsDuplicates(new int[] { 9, 8, 7, 3, 6, 5, 1, 0, 4, 2 });
+            var expected1 = true;
+            var expected2 = false;
+            var expected3 = true;
+            var expected4 = false;
+            Assert.Equal(expected1, result1);
+            Assert.Equal(expected2, result2);
+            Assert.Equal(expected3, result3);
+            Assert.Equal(expected4, result4);
+        }
     }
 }
