5fdbf3d7e56fa8648d5581229b47fe626f4ed8d4
Bartosz Sitek <bartosz.sitek.97@gmail.com>
2023-08-27 12:06:18 +0200

    added integer to binary kata with tests

---
+++ AlgorithmsKatas/IntegerToBinary.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgorithmsKatas
+{
+    //https://www.codewars.com/kata/55606aeebf1f0305f900006f/csharp
+    public class IntegerToBinary
+    {
+        public string ToBinaryV2(int n)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < 32; i++)
+            {
+                result = (n & 1) + result;
+                Console.WriteLine(n & 1);
+                Console.WriteLine(result);
+                n >>= 1;
+            }
+            int startindex = result.IndexOf('1');
+            return result.Substring(startindex);
+        }
+        public string ToBinary(int n) => Convert.ToString(n, 2);
+    }
+}
---
+++ AlgorithmsKatasTests/IntegerToBinaryTest.cs
@@ -0,0 +1,50 @@
+﻿using AlgorithmsKatas;
+using Xunit;
+
+namespace AlgorithmsKatasTests
+{
+
+    public class IntegerToBinaryTest
+    {
+        private readonly IntegerToBinary _sut;
+        public IntegerToBinaryTest()
+        {
+            _sut = new IntegerToBinary();
+        }
+
+        [Fact]
+        public void IntegerToBinaryTests()
+        {
+
+            var result = _sut.ToBinary(2);
+            var result2 = _sut.ToBinary(3);
+            var result3 = _sut.ToBinary(4);
+            var result4 = _sut.ToBinary(-3);
+            var result5 = _sut.ToBinary(-4);
+            var result6 = _sut.ToBinary(-8);
+            Assert.Equal("10", result);
+            Assert.Equal("11", result2);
+            Assert.Equal("100", result3);
+            Assert.Equal("11111111111111111111111111111101", result4);
+            Assert.Equal("11111111111111111111111111111100", result5);
+            Assert.Equal("11111111111111111111111111111000", result6);
+        }
+        [Fact]
+        public void IntegerToBinaryTestsV2()
+        {
+
+            var result = _sut.ToBinaryV2(2);
+            var result2 = _sut.ToBinaryV2(3);
+            var result3 = _sut.ToBinaryV2(4);
+            var result4 = _sut.ToBinaryV2(-3);
+            var result5 = _sut.ToBinaryV2(-4);
+            var result6 = _sut.ToBinaryV2(-8);
+            Assert.Equal("10", result);
+            Assert.Equal("11", result2);
+            Assert.Equal("100", result3);
+            Assert.Equal("11111111111111111111111111111101", result4);
+            Assert.Equal("11111111111111111111111111111100", result5);
+            Assert.Equal("11111111111111111111111111111000", result6);
+        }
+    }
+}
