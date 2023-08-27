8c63db9854448efbffa6daee758cb2a8227f4b02
Bartosz Sitek <bartosz.sitek.97@gmail.com>
2023-08-28 00:06:26 +0200

    added letter combinations kata with tests

---
+++ AlgorithmsKatas/LetterCombinations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsKatas
+{
+    //https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+    public class LetterCombinations
+    {
+        private Dictionary<char, string> digitToLetters = new Dictionary<char, string>
+    {
+
+        { '2', "abc" },
+        { '3', "def" },
+        { '4', "ghi" },
+        { '5', "jkl" },
+        { '6', "mno" },
+        { '7', "pqrs" },
+        { '8', "tuv" },
+        { '9', "wxyz" }
+    };
+
+        public IList<string> LetterCombination(string digits)
+        {
+            IList<string> result = new List<string>();
+            if (string.IsNullOrWhiteSpace(digits))
+                return result;
+
+            void Combinations(int index, string path)
+            {
+                if (index == digits.Length)
+                {
+                    result.Add(path);
+                    return;
+                }
+
+                char digit = digits[index];
+                string letters = digitToLetters[digit];
+                foreach (char letter in letters)
+                {
+                    Combinations(index + 1, path + letter);
+                }
+            }
+
+            Combinations(0, "");
+            return result;
+        }
+    }
+}
---
+++ AlgorithmsKatasTests/LetterCombinationsTest.cs
@@ -0,0 +1,36 @@
+﻿using AlgorithmsKatas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AlgorithmsKatasTests
+{
+    public class LetterCombinationsTest
+    {
+        private readonly LetterCombinations _sut;
+        public LetterCombinationsTest()
+        {
+            _sut = new LetterCombinations();
+        }
+        [Fact]
+        public void LetterCombinationsOfPNTests()
+        {
+            var result1 = _sut.LetterCombination("23");
+            var result2 = _sut.LetterCombination("");
+            var result3 = _sut.LetterCombination("2");
+            List<string> expected1 = new List<string>
+            {
+                "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"
+            };
+            List<string> expected2 = new List<string>();
+            List<string> expected3 = new List<string> { "a", "b", "c" };
+            Assert.Equal(expected1, result1);
+            Assert.Equal(expected2, result2);
+            Assert.Equal(expected3, result3);
+        }
+    }
+}
