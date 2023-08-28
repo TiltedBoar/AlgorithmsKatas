b7ef1e8cc4dd186e6b6cb0faeb524a6d2af2e9ff
Bartosz Sitek <bartosz.sitek.97@gmail.com>
2023-08-28 10:53:19 +0200

    added move zeroes kata with tests

---
+++ AlgorithmsKatas/MoveZeroes.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgorithmsKatas
+{
+    public class MoveZeroes
+    {
+        //https://leetcode.com/problems/move-zeroes/
+        public void MoveZeroesToEndOfList(int[] nums)
+        {
+            int count = 0; //tracking index position where is the zero
+            for (int i = 0; i < nums.Length; i++) //iterate through array
+            {
+                if (nums[i] != 0)
+                {
+                    (nums[count], nums[i]) = (nums[i], nums[count]);
+                    //if it is not zero, swap numbers based on the index count,  e.g 0,1,0,3,12, where 'count' is left side, and 'i' is right side
+                    //zeroes are tracked by count
+                    count++;
+                }
+
+            }
+        }
+    }
+}
---
+++ AlgorithmsKatasTests/MoveZeroesTest.cs
@@ -0,0 +1,50 @@
+﻿using AlgorithmsKatas;
+using Xunit;
+
+namespace AlgorithmsKatasTests
+{
+    public class MoveZeroesTest
+    {
+        private readonly MoveZeroes _sut;
+        public MoveZeroesTest()
+        {
+            _sut = new MoveZeroes();
+        }
+        [Fact]
+        public void ShouldMoveZerosToEnd()
+        {
+
+            int[] nums = { 0, 1, 0, 3, 12 };
+            int[] expected = { 1, 3, 12, 0, 0 };
+            _sut.MoveZeroesToEndOfList(nums);
+            Assert.Equal(expected, nums);
+        }
+
+        [Fact]
+        public void ShouldHandleSingleZero()
+        {
+            int[] nums = { 0 };
+            int[] expected = { 0 };
+            _sut.MoveZeroesToEndOfList(nums);
+            Assert.Equal(expected, nums);
+        }
+
+        [Fact]
+        public void ShouldHandleNoZeros()
+        {
+            int[] nums = { 1, 2, 3, 4, 5 };
+            int[] expected = { 1, 2, 3, 4, 5 };
+            _sut.MoveZeroesToEndOfList(nums);
+            Assert.Equal(expected, nums);
+        }
+
+        [Fact]
+        public void ShouldHandleAllZeros()
+        {
+            int[] nums = { 0, 0, 0, 0 };
+            int[] expected = { 0, 0, 0, 0 };
+            _sut.MoveZeroesToEndOfList(nums);
+            Assert.Equal(expected, nums);
+        }
+    }
+}
