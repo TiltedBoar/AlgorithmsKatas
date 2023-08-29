9b7a4bbfd0acc16b360f6eb97855979c1c5ab542
Bartosz Sitek <bartosz.sitek.97@gmail.com>
2023-08-29 14:19:38 +0200

    Binary tree path kata with tests

---
+++ AlgorithmsKatas/BinaryTreePath.cs
@@ -0,0 +1,87 @@
+﻿namespace AlgorithmsKatas
+{
+    //https://leetcode.com/problems/binary-tree-paths/
+
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class BinaryTreePath
+    {
+        public List<string> paths = new List<string>();
+
+        public IList<string> BinaryTreePaths(TreeNode root)
+        {
+            GeneratePath(paths, root, "");
+            return paths;
+        }
+
+        public void GeneratePath(List<string> result, TreeNode? root, string currentPath)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            currentPath = $"{currentPath}{root.val}"; //current path is a string, is not mutable, thus there is created a new instance every single time
+            if (root.left is null && root.right is null) // if both are null, then add current path and then go to the other side of the tree
+            {
+                result.Add(currentPath);
+            }
+
+            var arrowPath = $"{currentPath}->";
+            GeneratePath(result, root.left, arrowPath);
+            GeneratePath(result, root.right, arrowPath);
+        }
+
+
+
+        ////right>root>left
+        //public void GeneratePath2(List<string> result, TreeNode? root, string currentPath)
+        //{
+        //    if (root == null)
+        //    {
+        //        return;
+        //    }
+        //    currentPath = $"{currentPath}{root.val}";
+        //    var arrowPath = $"{currentPath}->";
+        //    GeneratePath(result, root.right, arrowPath);
+        //    if (root.left is null && root.right is null)
+        //    {
+        //        result.Add(currentPath);
+        //    }
+
+
+        //    GeneratePath(result, root.left, arrowPath);
+
+        //}
+
+        //// left>root>right
+        //public void GeneratePat3(List<string> result, TreeNode? root, string currentPath)
+        //{
+        //    if (root == null)
+        //    {
+        //        return;
+        //    }
+
+        //    currentPath = $"{currentPath}{root.val}";
+        //    var arrowPath = $"{currentPath}->";
+        //    GeneratePath(result, root.left, arrowPath);
+        //    if (root.left is null && root.right is null)
+        //    {
+        //        result.Add(currentPath);
+        //    }
+        //    GeneratePath(result, root.right, arrowPath);
+
+        //}
+    }
+}
---
+++ AlgorithmsKatasTests/BinaryTreePathTest.cs
@@ -0,0 +1,184 @@
+﻿using AlgorithmsKatas;
+using Xunit;
+
+namespace AlgorithmsKatasTests
+{
+    public class BinaryTreePathTest
+    {
+        private readonly BinaryTreePath _sut;
+        public BinaryTreePathTest()
+        {
+            _sut = new BinaryTreePath();
+        }
+        [Fact]
+        public void BinaryTreePath_ReturnsCorrectPaths()
+        {
+
+            var root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.right = new TreeNode(5);
+
+            var expectedPaths = new List<string> { "1->2->5", "1->3" };
+
+            var result = _sut.BinaryTreePaths(root);
+            Assert.Equal(expectedPaths, result);
+        }
+
+        [Fact]
+        public void BinaryTreePath_ReturnsSingleRootNode()
+        {
+
+            var root = new TreeNode(42);
+            var expectedPath = new List<string> { "42" };
+            var result = _sut.BinaryTreePaths(root);
+            Assert.Equal(expectedPath, result);
+        }
+
+        [Fact]
+        public void BinaryTreePath_ReturnsEmptyListForNullRoot()
+        {
+
+            var result = _sut.BinaryTreePaths(null);
+            Assert.Empty(result);
+        }
+        [Fact]
+        public void BinaryTreePaths_ReturnsCorrectPaths()
+        {
+            var binaryTreePath = new BinaryTreePath();
+
+            var root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.right = new TreeNode(5);
+
+            var expectedPaths = new List<string> { "1->2->5", "1->3" };
+
+            var result = binaryTreePath.BinaryTreePaths(root);
+
+            Assert.Equal(expectedPaths, result);
+        }
+
+        [Fact]
+        public void BinaryTreePaths_ReturnsSingleRootNode()
+        {
+            var binaryTreePath = new BinaryTreePath();
+
+            var root = new TreeNode(42);
+
+            var expectedPath = new List<string> { "42" };
+
+            var result = binaryTreePath.BinaryTreePaths(root);
+
+            Assert.Equal(expectedPath, result);
+        }
+
+        [Fact]
+        public void BinaryTreePaths_ReturnsEmptyListForNullRoot()
+        {
+            var binaryTreePath = new BinaryTreePath();
+
+            var result = binaryTreePath.BinaryTreePaths(null);
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void BinaryTreePaths_ReturnsCorrectPaths_ForComplexTree()
+        {
+            var binaryTreePath = new BinaryTreePath();
+
+            var root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.left = new TreeNode(4);
+            root.left.right = new TreeNode(5);
+            root.right.left = new TreeNode(6);
+            root.right.right = new TreeNode(7);
+
+            var expectedPaths = new List<string>
+            {
+                "1->2->4",
+                "1->2->5",
+                "1->3->6",
+                "1->3->7"
+            };
+
+            var result = binaryTreePath.BinaryTreePaths(root);
+
+            Assert.Equal(expectedPaths, result);
+        }
+
+        [Fact]
+        public void BinaryTreePaths_ReturnsEmptyList_ForEmptyTree()
+        {
+            var binaryTreePath = new BinaryTreePath();
+
+            var result = binaryTreePath.BinaryTreePaths(null);
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void BinaryTreePaths_ReturnsCorrectPaths_ForSingleBranchTree()
+        {
+            var binaryTreePath = new BinaryTreePath();
+
+            var root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.left.left = new TreeNode(3);
+            root.left.left.left = new TreeNode(4);
+
+            var expectedPaths = new List<string>
+            {
+                "1->2->3->4"
+            };
+
+            var result = binaryTreePath.BinaryTreePaths(root);
+
+            Assert.Equal(expectedPaths, result);
+        }
+
+        [Fact]
+        public void BinaryTreePaths_ReturnsCorrectPaths_ForDuplicateValues()
+        {
+
+            var root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(2);
+            root.left.left = new TreeNode(3);
+            root.left.right = new TreeNode(4);
+
+            var expectedPaths = new List<string>
+            {
+                "1->2->3",
+                "1->2->4",
+                "1->2"
+            };
+
+            var result = _sut.BinaryTreePaths(root);
+
+            Assert.Equal(expectedPaths, result);
+        }
+
+        [Fact]
+        public void BinaryTreePaths_ReturnsCorrectPaths_ForNegativeValues()
+        {
+            var root = new TreeNode(-1);
+            root.left = new TreeNode(-2);
+            root.right = new TreeNode(-3);
+            root.left.left = new TreeNode(-4);
+
+            var expectedPaths = new List<string>
+            {
+                "-1->-2->-4",
+                "-1->-3"
+            };
+
+            var result = _sut.BinaryTreePaths(root);
+
+            Assert.Equal(expectedPaths, result);
+        }
+    }
+}
+
