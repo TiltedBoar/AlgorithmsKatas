b3401a6e2e5c1bfcd718100eb5712f629414c58f
Bartosz Sitek <bartosz.sitek.97@gmail.com>
2023-08-29 20:02:37 +0200

    added balanced tree path kata with tests

---
+++ AlgorithmsKatas/BalancedTreePath.cs
@@ -0,0 +1,72 @@
+﻿namespace AlgorithmsKatas
+{
+    //https://leetcode.com/problems/balanced-binary-tree/
+    public class BalancedBinaryTree
+    {
+
+        // Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public bool IsBalanced(TreeNode root)
+        {
+            if (root == null) return true;
+            int treeHeight = Height(root);
+            return treeHeight >= 0;
+        }
+
+        //-1 child is imbalanced
+        private int Height(TreeNode node)
+        {
+            int height;
+            if (node == null)
+            {
+                return 0;
+            }
+            //calculate height of the children
+            var leftChildHeight = Height(node.left);
+            var rightChildHeight = Height(node.right);
+            //if child is not balanced, mark it the tree as unbalanced, exit the code immediately if it is -1
+            if (leftChildHeight == -1 || rightChildHeight == -1)
+            {
+                return -1;
+            }
+
+            //check if left node is not balanced
+            if ((leftChildHeight - rightChildHeight) > 1)
+            {
+                return -1;
+            }
+            //check if right node is not balanced
+            if ((rightChildHeight - leftChildHeight) > 1)
+            {
+
+                return -1;
+            }
+
+            //return the tallest child and increment by one (+1 because only then it will become height of the parent)
+            if (leftChildHeight > rightChildHeight)
+            {
+                height = 1 + leftChildHeight;
+            }
+            else
+            {
+                height = 1 + rightChildHeight;
+            }
+
+            return height;
+
+        }
+    }
+}
+
---
+++ AlgorithmsKatasTests/BalancedTreePathTest.cs
@@ -0,0 +1,135 @@
+﻿using AlgorithmsKatas;
+using Xunit;
+
+namespace AlgorithmsKatasTests
+{
+    public class BalancedBinaryTreeTest
+    {
+        private readonly BalancedBinaryTree _sut;
+
+        public BalancedBinaryTreeTest()
+        {
+            _sut = new BalancedBinaryTree();
+        }
+
+        [Fact]
+        public void GivenTreeNodes_When_TreeIsBalancedWithDifferentHeight_Then_ReturnTrue()
+        {
+            // Simple balanced tree:       1
+            //                            / \
+            //                           2   2
+            //                          / \
+            //                         3   3
+            var root = new BalancedBinaryTree.TreeNode(1, new BalancedBinaryTree.TreeNode(2, new BalancedBinaryTree.TreeNode(3),
+                    new BalancedBinaryTree.TreeNode(3)), new BalancedBinaryTree.TreeNode(2));
+
+            var result = _sut.IsBalanced(root);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GivenTreeNodes_When_TreeHasDifferentHeight_Then_ReturnTrue()
+        {
+            // Simple balanced tree with higher left subtree:
+            //                             1
+            //                            / \
+            //                           2   2
+            //                          /
+            //                         3
+            var root = new BalancedBinaryTree.TreeNode(1, new BalancedBinaryTree.TreeNode(2, new BalancedBinaryTree.TreeNode(3),
+                    null), new BalancedBinaryTree.TreeNode(2));
+            var result = _sut.IsBalanced(root);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GivenTreeNodes_When_TreeIsComplexAndBalanced_Then_ReturnTrue()
+        {
+            // Complex balanced tree:       1
+            //                            /   \
+            //                           2     2
+            //                          / \   / \
+            //                         3   4 4   3
+            var root = new BalancedBinaryTree.TreeNode(1, new BalancedBinaryTree.TreeNode(2, new BalancedBinaryTree.TreeNode(3),
+                new BalancedBinaryTree.TreeNode(4)), new BalancedBinaryTree.TreeNode(2, new BalancedBinaryTree.TreeNode(4), new BalancedBinaryTree.TreeNode(3)));
+
+            var result = _sut.IsBalanced(root);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GivenTreeNodes_When_TreeIsComplexAndBalancedWithLeftSubtreeHigher_ThenReturnTrue()
+        {
+            // Complex tree with higher left subtree:
+            //                              1
+            //                            /   \
+            //                           2     2
+            //                          / \   / \
+            //                         3   4 4   3
+            //                        /
+            //                       5
+            var root = new BalancedBinaryTree.TreeNode(1,
+                new BalancedBinaryTree.TreeNode(2, new BalancedBinaryTree.TreeNode(3, new BalancedBinaryTree.TreeNode(5), null),
+                new BalancedBinaryTree.TreeNode(4)), new BalancedBinaryTree.TreeNode(2, new BalancedBinaryTree.TreeNode(4), new BalancedBinaryTree.TreeNode(3)));
+
+            var result = _sut.IsBalanced(root);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GivenTreeNodes_WHen_TreeIsEmpty_Then_ReturnTrue()
+        {
+            BalancedBinaryTree.TreeNode root = null;
+
+            var result = _sut.IsBalanced(root);
+
+            Assert.True(result);
+        }
+        [Fact]
+        public void GivenTreeNodes_When_TreeIsUnbalancedWithLeftSubtreeHigher_Then_ReturnFalse()
+        {
+            // Unbalanced tree with higher left subtree:
+            //                          1
+            //                        /   \
+            //                       2     3
+            //                      / \   / \
+            //                     4   5 6   7
+            //                    /
+            //                   8
+            //                  /
+            //                 9
+            var root = new BalancedBinaryTree.TreeNode(1, new BalancedBinaryTree.TreeNode(2, new BalancedBinaryTree.TreeNode(4,
+                        new BalancedBinaryTree.TreeNode(8, new BalancedBinaryTree.TreeNode(9), null), null), new BalancedBinaryTree.TreeNode(5)),
+                        new BalancedBinaryTree.TreeNode(3, new BalancedBinaryTree.TreeNode(6), new BalancedBinaryTree.TreeNode(7)));
+
+            var result = _sut.IsBalanced(root);
+
+            Assert.False(result);
+        }
+        [Fact]
+        public void GivenTreeNodes_When_TreeIsUnbalancedWithRightSubtree_Then_ReturnFalse()
+        {
+            // Unbalanced with higher right subtree:
+            //                       1
+            //                      /   \
+            //                     2     3
+            //                        /   \
+            //                       4     5
+            //                        \
+            //                         6
+            //                          \
+            //                           7
+            var root = new BalancedBinaryTree.TreeNode(1, new BalancedBinaryTree.TreeNode(2), new BalancedBinaryTree.TreeNode(3, null,
+                    new BalancedBinaryTree.TreeNode(4, null, new BalancedBinaryTree.TreeNode(5, new BalancedBinaryTree.TreeNode(6, null,
+                                new BalancedBinaryTree.TreeNode(7)), null))));
+
+            var result = _sut.IsBalanced(root);
+
+            Assert.False(result);
+        }
+    }
+}
